27b87e9b7d7634c2a31460918a1c1fa3e0e47797
AAA <aaa@aaa.com>
2016-11-20 17:40:21 -0600

    Finished Support Ticket GUI program

--- Support Ticket GUI/SupportTicketGUI.java
+++ Support Ticket GUI/SupportTicketGUI.java
@@ -1,3 +1,6 @@
+import java.text.*;
+import java.io.*;
+
 import javax.swing.event.*;
  
 import java.util.*;
@@ -7,8 +10,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+	
+
 //Notice that this class extends JPanel
 public class SupportTicketGUI extends JPanel{
+ static LinkedList<Ticket> searchResultsList = new LinkedList<Ticket>();
+	static LinkedList<Ticket> resolvedList = new LinkedList<Ticket>();
+    static LinkedList<Ticket> ticketQueue = new LinkedList<Ticket>();
+
+
 
     //Global variables for GUI components
     private JLabel descriptionLabel;
@@ -24,10 +34,10 @@ public class SupportTicketGUI extends JPanel{
 	private int priority;
 	private JSpinner prioritySpinner;
 	private JLabel priorityLabel;
-	
+//create a model for a spinner	
 	SpinnerNumberModel priorityModel = new SpinnerNumberModel(1,1,5,1);
 	
-    DefaultListModel<Ticket> ticketListModel;
+    static DefaultListModel<Ticket> ticketListModel;
 
     //Configure your GUI components in the constructor
     SupportTicketGUI() {
@@ -37,7 +47,7 @@ public class SupportTicketGUI extends JPanel{
         addComponents();
         configureListeners();
     }
-
+//This method is called in the GUI constructor
     private void addComponents() {
 
         priorityLabel = new JLabel("Choose a priority rating");
@@ -73,9 +83,6 @@ public class SupportTicketGUI extends JPanel{
         ticketJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         
-        //Layout of components can be tricky. By default, components are laid out in a horizontal line, one after the other.
-        // If that's not what you want, you'll need to create a layout manager to manage the position and size of your components.
-        // This GUI uses a combination of two Layout managers.
         //BorderLayout arranges components in the north, south, east, west, or center.
         //BoxLayout can arrange components in vertical or horizontal stack(s).
 
@@ -124,7 +131,7 @@ public class SupportTicketGUI extends JPanel{
 
 
     }
-
+//This method is called in the GUI constructor
     private void configureListeners() {
         addTicketToListButton.addActionListener(new ActionListener() {
             @Override
@@ -151,9 +158,11 @@ String reporter = reporterTextField.getText();
 				Date date = new Date();
 				// Create Ticket and add to JList's model
                 Ticket newTicket = new Ticket(description, priority, reporter, date);
-                ticketListModel.addElement(newTicket);
-
-				
+                //add new ticket to list model
+				ticketListModel.addElement(newTicket);
+				//add this ticket to the ticketQueue
+				ticketQueue.add(newTicket);
+				//clear both text fields
                 descriptionTextField.setText("");
                 reporterTextField.setText("");
 
@@ -177,8 +186,21 @@ String reporter = reporterTextField.getText();
                 // Notice since we've used generic types, setSelectedValue returns a Ticket.
                 // Without generic types, it would return an Object, and we'd have to cast it.
                 Ticket toDelete = ticketJList.getSelectedValue();
-                // Delete this Dog from the MODEL
-                ticketListModel.removeElement(toDelete);
+                
+				Ticket t;
+				for (int i=0; i<ticketQueue.size(); i++)
+				{
+					t=ticketQueue.get(i);
+					if (t.getID() == toDelete.getID())
+					{
+						ticketQueue.remove(i);
+					}
+				}
+				
+				//add this Ticket to resolve list for later writing to a file
+				resolvedList.add(toDelete);
+				//remove this ticket from the ticket list model
+				ticketListModel.removeElement(toDelete);
             }
         });
 
@@ -186,20 +208,27 @@ String reporter = reporterTextField.getText();
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                
+				//before quit, write open tickets to file
+				writeOpenTicketsToFile(ticketQueue);
+				//and resolved tickets
+				writeResolvedTicketsToFile(resolvedList);
+				
+				System.exit(0);
             }
         });
 
 
     }
 
-
+//Main method
     public static void main(String[] args) {
 
+			
+			
         //Create a frame - the GUI window
         JFrame ticketFrame = new JFrame("Support Ticket");
         //configure the JFrame / GUI window
-        //dogFrame.setSize(350, 200);
         ticketFrame.setVisible(true);
         ticketFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -210,8 +239,173 @@ String reporter = reporterTextField.getText();
 
         //And add it to the JFrame
         ticketFrame.add(panel);
+//call readticketsFromFile method to start where program last exited
+		readTicketsFromFile();
 
+		
         ticketFrame.pack();
 
     }
-}
+//This method takes all open tickets and writes them to OpenTickets.txt
+	public static void writeOpenTicketsToFile(LinkedList<Ticket> ticketQueue) {
+	
+	try{
+		FileWriter writer = new FileWriter("OpenTickets.txt");
+	
+		BufferedWriter bufWriter = new BufferedWriter(writer);
+
+
+		
+		
+        Ticket T;
+		for (int i=0; i<ticketQueue.size(); i++)
+		{
+		T=ticketQueue.get(i);
+		bufWriter.write(T.getID()+";");
+		bufWriter.write(T.getDescription()+";");
+		bufWriter.write(T.getReporter()+";");
+		bufWriter.write(T.getDate()+"\r\n");
+		
+		
+	
+	
+		}
+		bufWriter.write("Counter;"+Ticket.getStaticTicketID());
+		
+		
+        
+        bufWriter.close();
+	}
+	catch (Exception e){
+		System.out.println("Error");
+	}
+	
+	
+	}
+		//This method takes all tickets in resolved list and writes them to a text file
+	public static void writeResolvedTicketsToFile(LinkedList<Ticket> ticketQueue) {
+	
+	try{
+		DateFormat dateFormatter = null;
+
+		//create date object with today's date
+		Date d = new Date();
+		//create date formatter object
+		dateFormatter = new SimpleDateFormat("E MMM dd HH:mm:ssZ yyyy");			
+		//use date formatter on date object
+		String dateString = dateFormatter.format(d);
+		//have to replace : with another char because can't use : in Windows paths
+		dateString = dateString.replace(":",";");
+		//take the date string and make part of FileWriter file name
+		FileWriter writer = new FileWriter("Resolved_Tickets_As_of_"+dateString+".txt");
+	
+		BufferedWriter bufWriter = new BufferedWriter(writer);
+
+
+		
+		//create ticket object
+        Ticket T;
+		//each loop, assign ticket object T to every ticket in resolved list and write its info to the file
+		for (int i=0; i<ticketQueue.size(); i++)
+		{
+		T=ticketQueue.get(i);
+		bufWriter.write(T.getID()+";");
+		bufWriter.write(T.getDescription()+";");
+		bufWriter.write(T.getReporter()+";");
+		bufWriter.write(T.getDate()+"\r\n");
+		bufWriter.write(T.getResolveString()+"\r\n");
+		bufWriter.write(T.getResolveDate()+"\r\n");
+		
+		
+	
+	
+		}
+		
+		
+        
+        bufWriter.close();
+	}
+	catch (IOException e){
+		System.out.println("IO Error detected");
+	}
+	
+	
+	}
+		
+	
+	
+			//This method takes all written ticket information in OpenTickets.txt and makes ticket objects out of it and places them in a LinkedList
+	public static void readTicketsFromFile()
+	{
+		 
+		 
+		 //credit for date formatting:  https://www.mkyong.com/java/how-to-convert-string-to-date-java/
+		 
+		 
+		 try{
+		 FileReader reader = new FileReader("OpenTickets.txt");
+        //BufferedReader combines the characters read into whole lines.
+        BufferedReader bufReader = new BufferedReader(reader);
+//create date format object
+		 DateFormat dateFormatter = null;
+
+//read in first line
+		String line = bufReader.readLine();
+
+        //while line is not blank
+        while (line != null) {
+            //take the current line and split it into elements at each ;
+			String[] elements = line.split(";+");
+			//if the current line is not the static counter line
+			if (elements[0].equals ("Counter"))
+			{
+			int s = Integer.parseInt(elements[1]);
+			Ticket.setStaticTicketCounter(s); //pick up where left off ticket counting
+	
+			}
+			else
+			{
+			//take all elements and use them to create a ticket object
+			int newID = Integer.parseInt(elements[0]);
+			
+			
+			String stringDate = elements[3];
+
+			//Have to convert date string into a Date object
+ dateFormatter = new SimpleDateFormat("E MMM dd HH:mm:ssZ yyyy");			
+
+Date date = (Date) dateFormatter.parse(stringDate);
+
+//create the ticket from this line
+			Ticket T = new Ticket(elements[1], newID, elements[2], date);
+			
+			//add this ticket to the listModel so it shows up in the GUI on open
+			ticketListModel.addElement(T);
+			
+			
+			//ticketQueue.add(T);	
+			
+			
+			}
+			// And then read in the next line ...
+            line = bufReader.readLine();
+			
+        }
+
+        	
+		 }//end of try block
+			catch (ParseException e)  {
+				System.out.println("There was a ParseException detected.");
+			}
+			
+			catch (IOException e){
+					System.out.println("There was an IOException detected.");
+			}
+			
+
+	}//end of read in from file method
+
+	
+	
+	
+	}
