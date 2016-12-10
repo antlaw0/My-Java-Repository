73dd098936e573736fd165a9ef6b8165c1a42f7e
AAA <aaa@aaa.com>
2016-12-10 13:31:57 -0600

    Final Project- can read player info from DB, create new char and add to DB too

--- Final Project/src/main/java/com/company/Game.java
+++ Final Project/src/main/java/com/company/Game.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.sql.*;
 
 import java.awt.*;       // Using AWT's Graphics and Color
 import java.awt.event.*; // Using AWT event classes and listener interfaces
@@ -15,9 +16,16 @@ public class Game
 		static Scanner stringScanner = new Scanner(System.in);
 		//Use this scanner to read in numerical data that will be stored in int or double variables
 		static Scanner numberScanner = new Scanner(System.in);
-static Player player = new Player("Guy","Brave and noble adventurer");
-		public static int x=0;
-public static int y=0;
+
+static Connection conn;		
+		static ResultSet rs;
+		static PreparedStatement psUpdatePlayerInfo;
+		static PreparedStatement psInsert;
+		static Statement statement;
+		
+static Player player;
+		public static int x;
+public static int y;
 public static LinkedList<String> itemMasterList= new LinkedList();
 		public static ArrayList<Room> roomList = new ArrayList<Room>();
 public static Room currentRoom;
@@ -25,22 +33,51 @@ static boolean displayedRoomInfo=false;
 
 public static void main(String[] args)
 {
-GUI gameGUI = new GUI();
+//set connection to SQL strings
+final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";        //Configure the driver needed
+    final String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/GameDB";     //Connection string – where's the database?
+    final String USER = "Test";   
+    final String PASSWORD = "password";   
+rs =null;
+statement=null;
+			try {
+
+            
+			Class.forName(JDBC_DRIVER);
+
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Can't instantiate driver class; check you have drives and classpath configured correctly?");
+            cnfe.printStackTrace();
+            System.exit(-1);  //No driver? Need to fix before anything else will work. So quit the program
+        }
+
+		try{
+	//create connection object conn
+			conn = DriverManager.getConnection(DB_CONNECTION_URL, USER, PASSWORD);
+            //create statement object from connection object
+			statement = conn.createStatement();
+			//create insert statement from connection object 
+			psInsert = conn.prepareStatement("INSERT INTO PlayerInfo VALUES (?,?)");
+			//create prepared statement from connection object
+			 psUpdatePlayerInfo=conn.prepareStatement("UPDATE PlayerInfo SET Description=?,Xpos=?,Ypos=?,HP=?,MaxHP=?,SP=?,MaxSP=?,MP=?,MaxMP=?  WHERE Name=?");
+			 String createTableSQL = "CREATE TABLE IF NOT EXISTS PlayerInfo (Name varchar(90), Description varchar(255), Xpos int, Ypos int, HP int, MaxHP int, SP int, MaxSP int, MP int, MaxMP int)";
+            statement.executeUpdate(createTableSQL);
+            
+			 
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+	
 Consumable test = new Consumable("Test Potion", "A test potion", 4,3,2);
 Consumable apple = new Consumable("Apple","A shiny, red apple",1,1,1);
 Armor testHelmet = new Armor("Helmet","A test helmet",1,5);
 Armor testTorsoArmor = new Armor("Test Armor","A test torso armor",2,5);
 Armor startingTorsoArmor = new Armor("Leather Armor","A leather torso armor",2,5);
-player.torsoSlot=startingTorsoArmor;
 Armor startingHelmet = new Armor("Leather Helmet","A test helmet",1,5);
-player.headSlot=startingHelmet;
 Armor startingLegArmor = new Armor("Leather Leggings","A pair of leather leggings",3,2);
-player.legSlot=startingLegArmor;
 Armor startingFeetArmor = new Armor("Leather Boots","A pair of leather boots",4,1);
-player.feetSlot=startingFeetArmor;
 
 
-player.inventory.add(test);
 String command;
 Room startingRoom = new Room("Starting Room", 0,0,"This is a large blank room... for now",false);
 NPC Bob = new NPC("Bob","An old man in a travelling cloak stands here.");
@@ -59,6 +96,31 @@ Room testRoom = new Room("A test room",5,5,"This is a testing room.",false);
 roomList.add(testRoom);
 
 System.out.println("Welcome to the game!");
+
+//find character in DB while loop
+boolean haveCharacter=false;
+String charName;
+while (haveCharacter==false)
+{
+	System.out.println("Enter the name of your character or type 'new' to start a new character:  ");
+	charName=stringScanner.nextLine();
+	if (charName.equals ("new")) {
+		haveCharacter=true;
+	}//end of if new character
+	if (searchForPlayerInDB(charName)) {
+		System.out.println("Loading "+charName);
+		haveCharacter=true;
+	}//end of if statement searchfor char in DB
+}//end of finding character loop
+player.torsoSlot=startingTorsoArmor;
+player.headSlot=startingHelmet;
+player.legSlot=startingLegArmor;
+player.feetSlot=startingFeetArmor;
+player.inventory.add(test);
+
+
+GUI gameGUI = new GUI();
+
 //game while loop
 while (true)
 {
@@ -319,5 +381,55 @@ public static void quitGame() {
 			else{System.out.println("You do not have that item.");}
 		
 	}//end of equip item method
+		public static boolean searchForPlayerInDB(String searchName)
+	{
+		    boolean foundMatch=false;
+			
+try {	
+	//make string containing the query
+	String fetchAllDataSQL = "SELECT * FROM PlayerInfo";
+            //create resultset with this query
+			rs = statement.executeQuery(fetchAllDataSQL);
+			//while there are results in the results set
+			while (rs.next()) {
+                //get string name from this result
+				String name = rs.getString("name");
+                if (name.equals (searchName))
+				{
+					String description=rs.getString("Description");
+					//set global x position to read-in value
+					int x=rs.getInt("Xpos");
+					//set global y position to read-in value
+					
+					int y=rs.getInt("Ypos");
+					int hp=rs.getInt("HP");
+					int maxhp=rs.getInt("MaxHP");
+					int sp=rs.getInt("SP");
+					int maxsp=rs.getInt("MaxSP");
+					int mp=rs.getInt("MP");
+					int maxmp=rs.getInt("MaxMP");
+					
+					player= new Player(name,description);
+					player.HP=hp;
+					player.maxHP=maxhp;
+					player.SP=sp;
+					player.maxSP=maxsp;
+					player.MP=mp;
+					player.maxMP=maxmp;
+				
+					System.out.println(name+" found. Starting game...");
+					foundMatch=true;
+				}//end of if name exists
+			}//end of while loop
+            
+	
+	
+} catch (SQLException se) {
+System.out.println("SQL Exception detected.");
+}	
+	
+	return foundMatch;
 		
+	}//end of read in from DB method
+
 }//end of game class
